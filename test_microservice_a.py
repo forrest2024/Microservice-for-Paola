f7d701c03bcd97481ed839c0ae0e0a393c3fd86e
forrest2024 <forrestmichael@protonmail.com>
2024-08-05 13:42:22 -0700

    commit

--- test_microservice_a.py
+++ test_microservice_a.py
@@ -1,11 +1,16 @@
 import zmq
 
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+def request_weather_data():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
 
-data = input("Enter city name: ")
-socket.send_string(data)
+    data = input("Enter city name: ")
+    socket.send_string(data)
 
-reply = socket.recv_pyobj()
-print(reply)
+    reply = socket.recv_pyobj()
+    return reply
+
+weather_data = request_weather_data()
+print(weather_data)
+
